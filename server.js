b83743f2216cf95663c313f7238fd2ada4640887
Shmee <smaheilman@gmail.com>
2022-01-02 19:57:17 -0500

    no public folder

--- server.js
+++ server.js
@@ -5,16 +5,41 @@ const app = express();
 const fs = require('fs');
 const path = require('path');
 
-const {notes} = require('./Develop/db/db.json');
+const notes = require("./db/db.json");
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/api/notes', (req,res) => {
     let results = notes
     console.log(notes);
     res.json(results);
+});
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
